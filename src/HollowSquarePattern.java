1ee6edc5e1298463b2e82ea3c7407e4962c381f2
AKASH RAI <120483379+Akashraidev@users.noreply.github.com>
2023-09-30 23:23:41 +0530

    Add files via upload

---
+++ src/HollowSquarePattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class HollowSquarePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int size = scanner.nextInt();
+
+        // Outer loop for rows
+        for (int i = 1; i <= size; i++) {
+            // Inner loop for columns
+            for (int j = 1; j <= size; j++) {
+                // Check if it's the first or last row, or the first or last column
+                // to print '*' for the border, and ' ' for the inside
+                if (i == 1 || i == size || j == 1 || j == size) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+
+        scanner.close();
+    }
+}
