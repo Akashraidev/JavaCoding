cf9f6cd0d8dc444b3664477382aaee5ec668f2a8
AKASH RAI <120483379+Akashraidev@users.noreply.github.com>
2023-10-22 23:28:49 +0530

    Update JavaCurrencyFormatter.java

--- src/JavaCurrencyFormatter.java
+++ src/JavaCurrencyFormatter.java
@@ -1,32 +1,20 @@
 import java.util.*;
 import java.text.*;
-
     public class JavaCurrencyFormatter {
-
         public static void main(String[] args) {
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter num");
             double payment = scanner.nextDouble();
-            scanner.close();
 
-            Locale usLocale = new Locale("en", "US");
-            Locale indiaLocale = new Locale("en", "IN");
-            Locale chinaLocale = Locale.CHINA;
-            Locale franceLocale = Locale.FRANCE;
+            NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+            NumberFormat india = new DecimalFormat("###,###.00");
+            NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+            NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
 
-            NumberFormat us = NumberFormat.getCurrencyInstance(usLocale);
-            NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
-            NumberFormat china = NumberFormat.getCurrencyInstance(chinaLocale);
-            NumberFormat france = NumberFormat.getCurrencyInstance(franceLocale);
-
-            String usFormatted = us.format(payment);
-            String indiaFormatted = india.format(payment);
-            String chinaFormatted = china.format(payment);
-            String franceFormatted = france.format(payment);
-
-            System.out.println("US: " + usFormatted);
-            System.out.println("India: " + indiaFormatted);
-            System.out.println("China: " + chinaFormatted);
-            System.out.println("France: " + franceFormatted);
+            System.out.println("US: " + us.format(payment));
+            System.out.println("India: Rs." + india.format(payment));
+            System.out.println("China: " + china.format(payment));
+            System.out.println("France: " + china.format(payment));
         }
     }
