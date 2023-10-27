1daf0203e500f7e10b7f9652e1602837ac437adf
AKASH RAI <120483379+Akashraidev@users.noreply.github.com>
2023-10-28 00:06:07 +0530

    Trim() Example Easy
    Trim() Example Easy

---
+++ src/TrimMethodExampleEasy.java
@@ -0,0 +1,8 @@
+public class TrimMethodExampleEasy {
+    public static void main(String[] args) {
+        String text = "   This is a simple example of trim() method.   ";
+        String trimmedText = text.trim();
+        System.out.println("Original Text: \"" + text + "\"");
+        System.out.println("Trimmed Text: \"" + trimmedText + "\"");
+    }
+}
